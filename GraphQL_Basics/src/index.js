6043d354ec38bcf52c40f0b2d65e16829874b3e2
Aman Sharma <amandeep.xml@gmail.com>
2019-09-10 21:34:58 -0400

    tested first gql query

--- GraphQL_Basics/src/index.js
+++ GraphQL_Basics/src/index.js
@@ -1,1 +1,21 @@
-console.log("hello gql");
+import { GraphQLServer } from "graphql-yoga";
+
+const typeDefs = `
+type Query {
+  hello: String!
+}
+`;
+const resolvers = {
+  Query: {
+    hello() {
+      return "Aman is here";
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => console.log("Server hasbeen started!!"));
