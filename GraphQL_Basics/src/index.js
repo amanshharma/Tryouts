90655022f8fef299dc25d2da95a8dc841f66f062
Aman Sharma <amandeep.xml@gmail.com>
2019-10-09 21:18:12 -0400

    gql new resolver update

--- GraphQL_Basics/src/index.js
+++ GraphQL_Basics/src/index.js
@@ -3,12 +3,27 @@ import { GraphQLServer } from "graphql-yoga";
 const typeDefs = `
 type Query {
   hello: String!
+  country: String!
+  greeting(name: String): String!
+  add(a: Float, b: Float): Float!
+
 }
 `;
 const resolvers = {
   Query: {
     hello() {
       return "Aman is here";
+    },
+    country() {
+      return "India";
+    },
+    greeting(parent, args, ctx, info) {
+      console.log("args", args);
+      return "Hey yo";
+    },
+    add(parent, args, ctx, info) {
+      const { a, b } = args;
+      return a + b;
     }
   }
 };
